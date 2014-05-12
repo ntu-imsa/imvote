070b4a721f1e72eb6611ca181c315f9301d5cd92
Shouko <h999342@gmail.com>
2014-05-12 16:25:45 +0800

    create settings.php to protect facebook api key

--- lib/header.php
+++ lib/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
   <head>
-    <title>IM Your Angel</title>
+    <title>資管好爸媽 ❤</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!--    <link href="http://bootswatch.com/cyborg/bootstrap.min.css" rel="stylesheet" media="screen"> -->
