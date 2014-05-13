4ef909940fcba21940a58eee9cc094e4577e535a
Shouko <h999342@gmail.com>
2014-05-13 12:05:08 +0800

    fix candidate-box wrap problems

--- candidate.php
+++ candidate.php
@@ -12,20 +12,14 @@ if(!isset($_GET['type'])||$_GET['type']!='mom'){
 $linkActive = 1;
 include './lib/header.php';
 ?>
-<table>
-	<tr>
 
 <?php for($i=0; $i<4; $i++){ ?>
-<td>
 <div class="candidate-box">
 <div class="candidate-name">B02 涂靖雯</div>
 <img class="img-circle" height="150" width="150" src="http://graph.facebook.com/jingwen.tu/picture?type=large">
 <a role="button" class="btn btn-danger vote-btn active"><i class="icon-ok-sign icon-white"></i> 投給她</a>
 </div>
-</td>
 <?php } ?>
-</tr>
-</table>
 <?php
 include './lib/footer.php';
 ?>
